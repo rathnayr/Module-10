3eee111f2304a227d65eb4093272bf0d88db4c39
rathnayr <rathnayr@metropolia.fi>
2024-10-27 18:39:12 +0530

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,1 +1,13 @@
+from building import Building
 
+building = Building(1, 10, 4)
+
+building.operate_elevator(0,8)
+
+building.operate_elevator(1, 4)
+
+building.operate_elevator(2, 3)
+
+building.operate_elevator(3, 5)
+
+building.reset_all_elevators()
